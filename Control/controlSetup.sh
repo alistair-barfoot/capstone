fe25ab400a43a505f4d84d59595a2d8408efbeb0
Abby VanderBerg <abbyjvanderberg@gmail.com>
2026-03-10 09:42:59 -0400

    Environment setup

---
+++ Control/controlSetup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+source armControl/bin/activate
+
+source ~/unitree_ros2/setup_default.sh
+
+export PATH=/opt/openrobots/bin:$PATH
+export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/opt/openrobots/lib/python3.10/site-packages:$PYTHONPATH
+export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH
